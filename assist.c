a088c8578dda9b89d8f6dcd8bd23c94b38b480bd
Jossydre <makinwadarejossy@gmail.com>
2023-12-13 16:54:24 -0800

    assist

--- assist.c
+++ assist.c
@@ -1,102 +1,46 @@
-#include "shell.h"
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * tokenize - to tokenizes input then stores in an array
- *@input_strings: inputs to be parsed
- *@delims: delimiter used, just one character string
+ *main - example of fork
  *
- *Return: array of the tokens
+ *Return: Always 0.
  */
-
-char **tokenize(char *input_strings, char *delims)
+int main(void)
 {
-	int num_delims = 0;
-	char **av = NULL;
-	char *tokens = NULL;
-	char *save_ptrs = NULL;
+	pid_t call_pid;
+	pid_t childs_pid;
+	pid_t parents_pid;
+	pid_t pid;
 
-	tokens = _strtok_s(input_strings, delims, &save_ptrs);
+	pid = fork();
 
-	while (tokens != NULL)
+	if (pid == -1)
 	{
-		av = _reallocs(av, sizeof(*av) * num_delims, sizeof(*av) * (num_delims + 1));
-		av[num_delims] = tokens;
-		tokens = _strtok_s(NULL, delims, &save_ptrs);
-		num_delims++;
+		perror("Error:");
+		return (1);
 	}
 
-	av = _reallocs(av, sizeof(*av) * num_delims, sizeof(*av) * (num_delims + 1));
-	av[num_delims] = NULL;
-
-	return (av);
-}
-
-/**
- *prints - prints strings to stdout
- *@strings: string that will be printed
- *@streams: stream that is print out to
- *
- *Return: void, return nothing
- */
-void prints(char *strings, int streams)
-{
-	int i = 0;
-
-	for (; strings[i] != '\0'; i++)
-		write(streams, &strings[i], 1);
-}
-
-/**
- *removes_newlines - dismisses new line from a string
- *@strs: string to be used
- *
- *Return: void
- */
-
-void removes_newlines(char *strs)
-{
-	int i = 0;
-
-	while (strs[i] != '\0')
+	if (pid == 0)
 	{
-		if (strs[i] == '\n')
-			break;
-		i++;
+		printf("After fork\n");
+
+		childs_pid = getpid();
+		call_pid = getppid();
+
+		printf("childs pid is %u\n", childs_pid);
+		printf("process pid is %u\n", call_pid);
 	}
-	strs[i] = '\0';
-}
+	else
+	{
+		printf("before fork\n");
 
-/**
- *_strcpys - copies string to another buffer
- *@sources: source to copy from
- *@desti: destination to copy to
- *
- * Return: void
- */
+		call_pid = getpid();
+		parents_pid = getppid();
 
-void _strcpys(char *sources, char *desti)
-{
-	int i = 0;
+		printf("process is %u\n", call_pid);
+		printf("bash pid(parents) is %u\n", parents_pid);
+	}
 
-	for (; sources[i] != '\0'; i++)
-		desti[i] = sources[i];
-	desti[i] = '\0';
-}
-
-/**
- *_strlens - counts the string length
- *@strings: string that will be counted
- *
- * Return: the string length
- */
-
-int _strlens(char *strings)
-{
-	int lens = 0;
-
-	if (strings == NULL)
-		return (lens);
-	for (; strings[lens] != '\0'; lens++)
-		;
-	return (lens);
+	return (0);
 }
