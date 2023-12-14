4b33425c1123bfb4845be0171eaf72b2dbf14c03
Jossydre <makinwadarejossy@gmail.com>
2023-12-13 17:16:36 -0800

    child process

---
+++ assist1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * fork - fork example
+ *
+ * Return: Always 0
+ */
+int fork(void)
+{
+	pid_t process_pid;
+	pid_t pid;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error:");
+		return (1);
+	}
+
+	process_pid = getpid();
+	printf("My pid is %u\n", process_pid);
+
+	if (pid == 0)
+	{
+		printf("(%u) Nooooo!\n", process_pid);
+	}
+	else
+	{
+		printf("(%u) %u, I am your Godfather\n", process_pid, pid);
+	}
+
+	return (0);
+}
